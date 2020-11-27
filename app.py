0dee8417a12642fa430fdd6d732f4d6ad0a0f04f
Winfried Loetzsch <winfried.loetzsch@teraki.com>
2020-11-27 17:18:33 +0100

    playing with streamlit

--- app.py
+++ app.py
@@ -1,24 +1,53 @@
 import numpy as np
 import streamlit as st
 from imageio import get_reader
+from detect_image import run_model, detect
+from utils.plots import plot_one_box
 
-from detect_image import detect_img
 
-st.title("Check the labels for all cars")
+@st.cache(suppress_st_warning=True)
+def yolo_compute(index):
+    frameX = np.array(video.get_data(index))
+    return run_model(frameX).numpy()
 
-submit = st.button('All correct')
-mistakes = st.button('Needs relabeling')
+
+def detect_class(y_config, classes, thresh):
+    y_config.classes = classes
+    y_config.conf_thres = thresh
+
+    bboxes = detect(y_config)
+
+    for bbox in bboxes:
+        label = f'{bbox[0]} {bbox[2]:.2f}'
+        plot_one_box(x=bbox[1], img=y_config.im0, label=label,
+                                    color=y_config.colors[y_config.names.index(bbox[0])])
+
+
+st.title("Check the labels for all objects")
 
 img_placeholder = st.empty()
-slider_ph = st.empty()
-
-if submit:
-    st.write("button pressed")
+frame_placeholder = st.sidebar.empty()
 
 video = get_reader("/home/winfried/pa_data/ananth.mp4")
-value = slider_ph.slider("model confidence threshold", 0.0, 1.0, 0.25, 0.01)
 
-frameX = np.array(video.get_data(0))
-result = detect_img(frameX, classes=[2], conf_thres=value)
+fs = frame_placeholder.slider("frame", 0, 1000, 0, 5)
+res = yolo_compute(fs).torch()  # , classes=[2], conf_thres=value
 
-img_placeholder.image(result, width=640)
+scl = st.sidebar.radio("show classes", ["all", "car", "pedestrians", "traffic lights"])
+tcar = st.sidebar.slider("car threshold", 0.0, 1.0, 0.25, 0.01)
+tped = st.sidebar.slider("pedestrians threshold", 0.0, 1.0, 0.25, 0.01)
+ttrl = st.sidebar.slider("traffic lights threshold", 0.0, 1.0, 0.25, 0.01)
+
+classes = []
+
+if scl == "car" or scl == "all":
+    detect_class(res, [2,7], tcar)
+if scl == "pedestrians" or scl == "all":
+    detect_class(res, [0], tped)
+if scl == "traffic lights" or scl == "all":
+    detect_class(res, [9], ttrl)
+
+img_placeholder.image(res.im0, width=640)
+
+submit = st.sidebar.button('All correct')
+mistakes = st.sidebar.button('Needs relabeling')
--- detect_image.py
+++ detect_image.py
@@ -1,5 +1,6 @@
 import argparse
 import time
+from copy import deepcopy
 from pathlib import Path
 
 import cv2
@@ -16,36 +17,49 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
-def detect(image_xx, opt):
-    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://'))
+class YoloConfig:
+    def __init__(self, pred, im0, img, names, colors):
+        self.img = img
+        self.im0 = im0
+        self.names = names
+        self.colors = colors
 
-    # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+        self.pred = pred
+        self.agnostic_nms = False
+        self.classes = None
+        self.iou_thres = 0.45
+        self.conf_thres = 0.25
+
+    def numpy(self):
+        self.pred = self.pred.cpu().numpy()
+        self.img = self.img.cpu().numpy()
+        return self
+
+    def torch(self):
+        xxx = deepcopy(self)
+        return deepcopy(YoloConfig(torch.tensor(xxx.pred).cuda(), xxx.im0,
+                          torch.tensor(xxx.img).cuda(), xxx.names, xxx.colors))
+
+
+def run_model(image_xx):
+    augment = False
+    weights = "yolov5x.pt"
+    imgsz = 640
+    device = "0"
 
     # Initialize
-    set_logging()
-    device = select_device(opt.device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
+    device = select_device(device)
 
     # Load model
     model = attempt_load(weights, map_location=device)  # load FP32 model
     imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
-    if half:
-        model.half()  # to FP16
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
+    random.seed(1)
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-    # Run inference
-    t0 = time.time()
-    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
-    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
-
-
+    # code from dataloader to scale image
     im0 = image_xx
     img = letterbox(image_xx, new_shape=imgsz)[0]
 
@@ -53,84 +67,40 @@ def detect(image_xx, opt):
     img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
     img = np.ascontiguousarray(img)
 
-
-
-
     img = torch.from_numpy(img).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img = img.float()  # uint8 to fp16/32
     img /= 255.0  # 0 - 255 to 0.0 - 1.0
     if img.ndimension() == 3:
         img = img.unsqueeze(0)
 
     # Inference
-    pred = model(img, augment=opt.augment)[0]
+    pred = model(img, augment=augment)[0]
+    return YoloConfig(pred, im0, img, names, colors)
 
+
+def detect(y_config):
     # Apply NMS
-    pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+    pred = non_max_suppression(y_config.pred, y_config.conf_thres, y_config.iou_thres, classes=y_config.classes,
+                               agnostic=y_config.agnostic_nms)
 
+    bboxes = []
     # Process detections
     for i, det in enumerate(pred):  # detections per image
-        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        gn = torch.tensor(y_config.im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
         if len(det):
             # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            det[:, :4] = scale_coords(y_config.img.shape[2:], det[:, :4], y_config.im0.shape).round()
 
             # Write results
             for *xyxy, conf, cls in reversed(det):
-                if save_txt:  # Write to file
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                bboxes.append((y_config.names[int(cls)], [el.item() for el in xyxy], conf.item()))
 
-                    # todo acc. labels here
-
-                # Add bbox to image
-                label = '%s %.2f' % (names[int(cls)], conf)
-                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-
-    return im0
-
-
-def detect_img(image, classes=None, conf_thres=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--save-img', action='store_true', help='save processed image or video files')
-
-    opt = parser.parse_args()
-    print(opt)
-
-    opt.weights = "yolov5x.pt"
-
-    if classes is not None:
-        opt.classes = classes
-
-    if conf_thres is not None:
-        opt.conf_thres = conf_thres
-
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-                return detect(image, opt)
-                strip_optimizer(opt.weights)
-        else:
-            return detect(image, opt)
+    return bboxes
 
 
 if __name__ == '__main__':
-    im = np.random.random((480,480,3))
-    res = detect_img(im)
+    im = np.random.random((480, 480, 3))
+    res = run_model(im)
+    res = detect(res)
     print(res)
